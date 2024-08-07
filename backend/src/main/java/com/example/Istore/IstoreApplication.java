d190deaa684e4cfe74eac81dcfa3cb18601fb6de
OlegKvadratniy <sacha.margelov@gmail.com>
2024-08-07 13:39:23 +0300

    First commit

---
+++ backend/src/main/java/com/example/Istore/IstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Istore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstoreApplication.class, args);
+	}
+
+}
